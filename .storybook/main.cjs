c05f2e9684121ed677f01fd0a036005975bd1fb9
Diego Fernandes <diego.schell.f@gmail.com>
2022-10-11 16:12:15 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-lab-design-system/'
+    }
+
+    return config
   }
 }
